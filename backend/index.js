ac6b8af7ddb8da10a382dfbe89740ff146aabf13
DESKTOP-O4T0U8E\erick <ekgimenes@gmail.com>
2021-10-28 23:35:13 -0300

    - Adding dependencies for database connection. - Adding get request to retrieve data from categories.

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(cors());
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'device_manager',
+    port:'3306'
+});
+
+db.connect(err=>{
+   if(err){ 
+    console.log('error while connecting to database!');
+} else{
+    console.log('connected to database!')
+}});
+
+app.listen(3000, ()=>{
+    console.log('running...');
+});
+
+app.get("/category", (req, res)=>{
+
+    let query = 'select * from category'
+    db.query =(query, (err, result)=>{
+        if(err){
+            console.log('error receiving data!')
+        }else{
+           if(result.listen > 0){
+               res.send({message: 'category', data: result});
+           }
+
+        }
+    });
+});
+
