221dbd748bdba692521ff138dd48817603db67bd
DESKTOP-O4T0U8E\erick <ekgimenes@gmail.com>
2021-11-03 21:24:15 -0300

    - Adding code reset navigation when tab changed.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private router: Router){}
   title = 'frontend';
+
+  tabChanged(changeEvent: MatTabChangeEvent) {
+    this.router.navigate(['/']);
+ } 
+
 }
